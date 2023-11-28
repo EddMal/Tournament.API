5017f4b0404a809c523b5297f6ba00cb88b60525
EddMal <107246914+EddMal@users.noreply.github.com>
2023-11-28 12:46:28 +0100

    Tournament controller and context.

---
+++ Tournament.API/Data/TournamentAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tournament.Core.Entities;
+
+namespace Tournament.API.Data
+{
+    public class TournamentAPIContext : DbContext
+    {
+        public TournamentAPIContext (DbContextOptions<TournamentAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tournament.Core.Entities.Tournament> Tournament { get; set; } = default!;
+    }
+}

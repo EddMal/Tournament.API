93cfe3032ab026af692ebb6a55daba513df5ba5d
EddMal <107246914+EddMal@users.noreply.github.com>
2023-11-28 11:27:36 +0100

    Added entities.

---
+++ Tournament.Core/Entities/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Entities
+{
+    public class Game
+    {
+        // KEY
+        public Guid Id { get; set; }
+    
+        // Add attribute.
+        public string Title { get; set; } = string.Empty;
+
+        // Add attribute.
+        public DateTime Time { get; set; }
+
+        //Foreign key
+        public Guid TournamentId;
+
+        //Navigation property
+        public Tournament Tournament { get; set; } = new Tournament();
+
+    }
+}
---
+++ Tournament.Core/Entities/Tournament.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Entities
+{
+    public class Tournament
+    {
+
+        // KEY
+        public Guid Id { get; set; }
+
+        // Add attribute.
+        public string? Title { get; set; }
+
+        // Add attribute.
+        public DateTime StartDate { get; set; }
+
+        //Navigation Property
+        ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}

24f203b347f614dbbe259c04b32f9a1c5ca17e3b
EddMal <107246914+EddMal@users.noreply.github.com>
2023-12-01 13:14:24 +0100

    Pause work on repository for the move of Services-files

--- Tournament.Core/Repositories/ITournamentRepository.cs
+++ Tournament.Core/Repositories/ITournamentRepository.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tournament.Core.DTO.TournamentDTO;
 
 namespace Tournament.Core.Repositories
 {
-    internal interface ITournamentRepository
+    public interface ITournamentRepository
     {
-        Task<IEnumerable<Tournament.Core.Entities.Tournament>> GetAllAsync();
-        Task<Tournament.Core.Entities.Tournament> GetAsync(int id);
-        Task<bool> AnyAsync(int id);
+        Task<IEnumerable<Tournament.Core.DTO.TournamentDTO.TournamentDTO>> GetAllAsync();
+        Task<TournamentDTO> GetAsync(Guid id);
+        Task<bool> AnyAsync(Guid id);
         void Add(Tournament.Core.Entities.Tournament tournament);
         void Update(Tournament.Core.Entities.Tournament tournament);
         void Remove(Tournament.Core.Entities.Tournament tournament);
--- Tournament.Data/Repositories/TournamentRepository.cs
+++ Tournament.Data/Repositories/TournamentRepository.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tournament.API.Data.Data;
+using Tournament.Core.DTO.TournamentDTO;
+using Tournament.Core.Repositories;
 
 namespace Tournament.Data.Repositories
 {
-    internal class TournamentRepository
+    public class TournamentRepository : ITournamentRepository
     {
+        private readonly TournamentAPIContext _context;
+
+        public TournamentRepository(TournamentAPIContext context)
+        {
+            _context = context;
+        }
+        //INSERT VERIFICATION AND RETURN BOOL IF SUCESS.
+        public void Add(Core.Entities.Tournament tournament)
+        {
+            _context.Tournaments.Add(tournament);
+            //await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AnyAsync(Guid id)
+        {
+            return await _context.Tournaments.AnyAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<TournamentDTO>> GetAllAsync()
+        {
+            var tournaments = await _context.Tournaments.ToListAsync();
+
+            if (tournaments.Count == 0)
+            {
+                return new List<TournamentDTO>();
+            }
+
+            List<TournamentDTO> TournamentDTOs = new List<TournamentDTO>();
+
+            foreach (var tournament in tournaments)
+            {
+                // change when made to a service :Tournament.API.Mappings.Mappings.TournamentToTournamentDTO(tournament);
+                TournamentDTO tournamentDTO = new TournamentDTO()
+                {
+                    Title = tournament.Title,
+                    StartDate = tournament.StartDate,
+                };
+                TournamentDTOs.Add(tournamentDTO);
+            }
+
+            return TournamentDTOs;
+        }
+
+        public async Task<TournamentDTO> GetAsync(Guid id)
+        {
+            Core.Entities.Tournament? tournament = await _context.Tournaments.FirstOrDefaultAsync(t => t.Id == id);
+            if (tournament == null)
+            {
+                //MESSAGE??
+                return new TournamentDTO();
+            }
+
+
+            //TournamentDTO tournamentDTO = Tournament.API.Mappings.Mappings.TournamentToTournamentDTO(tournament);
+            //return Tournament.API.Mappings.Mappings.TournamentToTournamentDTO(tournament);
+            return new TournamentDTO();
+
+        }
+
+        public void Remove(Core.Entities.Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Core.Entities.Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

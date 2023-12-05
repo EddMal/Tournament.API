8c925a46e1ef5677aac3c72da9ff54344b5f7ce4
EddMal <107246914+EddMal@users.noreply.github.com>
2023-12-05 15:04:29 +0100

    Work paused, assignment over.

---
+++ Tournament.Core/DTO/GameDTO/GameDTODisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.DTO.GameDTO
+{
+    internal class GameDTODisplay
+    {
+        // Update attribute.
+        [Required]
+        public string? Title { get; set; }
+
+        // Update attribute.
+        [Required]
+        public DateTime Time { get; set; }
+
+        // Update attribute.
+        public string? TournamentTitle { get; set; }
+    }
+}
